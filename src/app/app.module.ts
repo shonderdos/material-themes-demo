7df1d150bc01f7d6f10144977aed1c6ff881fd4b
Serge Honderdos <serge.honderdos@sytac.io>
2020-08-18 21:30:16 +0200

    Add bacis page in default theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//  Angular Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
